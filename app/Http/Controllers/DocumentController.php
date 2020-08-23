4ff44f5e7e3bc84d06d2c74c71348d168f5e6c4f
TakuyaNagai <nagataku021@outlook.jp>
2020-08-23 16:25:35 +0900

    帳票のような物が完成した

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -33,24 +33,27 @@ class DocumentController extends Controller
         // $data['test03'] = "03 - あいうえお - right";
 
         // $data=null;
-        $datas['date'] = [
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01",
-            "2020/08/01"
-        ];
-        $datas['equipments'] = ['機器A', '機器A', '機器A', '機器A', '機器A', '機器A'];
+        for($i=1;$i<31;$i++){
+            $date="2020/08/".$i;
+            $datas['date'][$i]=$date;
+        }
+        for($i=1;$i<15;$i++){
+            $equipment="機器".$i;
+            $datas['equipments'][$i]=$equipment;
+        }
+
+        for($i=1;$i<10;$i++){
+            $temp_type="温度";
+            $datas['temp_type'][$i]=$temp_type;
+        }
+
+        foreach($datas['equipments'] as $key => $equipment){
+            // $datas['temp'][$key]=array();
+            for($i=1;$i<30;$i++){
+                $temp="30℃";
+                $datas['temp'][$key][$i]=$temp;
+            }
+        }
         // PDF 生成メイン　－　A4 縦に設定
         $pdf = new TCPDF("L", "mm", "A4", true, "UTF-8" );
         $pdf->setPrintHeader(false);
@@ -64,7 +67,7 @@ class DocumentController extends Controller
 
         // ページ追加
         $pdf->addPage();
-        // error_log(print_r($datas,true),3, "/Users/takuya/myaaa/debug.log");
+        error_log(print_r($datas,true),3, "/Users/takuya/myaaa/debug.log");
         // HTMLを描画、viewの指定と変数代入 - pdf_test.blade.php
         $pdf->writeHTML(view("pdf_test")->with('datas', $datas)->render());
 
--- resources/views/pdf_test.blade.php
+++ resources/views/pdf_test.blade.php
@@ -17,15 +17,26 @@
 <!-- <body class="color_white"><table border="1" width="100%" cellpadding="10" cellspacing="10"> -->
 <div class="wrapper">
 <table class="tbl" border="1">
-<tr><th class="num">No.</th>
-  @foreach ($datas['equipments'] as $item)
-  <th>{{ $item }}</th>
+<tr><th class="num">日付\機器名称</th>
+  @foreach ($datas['equipments'] as $equipment)
+  <th>{{ $equipment }}</th>
+  @endforeach
+</tr>
+<tr><th class="num"></th>
+  @foreach ($datas['temp_type'] as $temp_type)
+  <th>{{ $temp_type }}</th>
   @endforeach
 </tr>
 
-@foreach ($datas['date'] as $key => $item)
-  <!-- <tr><td>{{ $key }}</td></tr> -->
-  <tr><td>{{ $item }}</td></tr>
+@foreach ($datas['date'] as$date)
+    <tr>
+      <td style="padding: 0px">{{ $date }}</td>
+  @foreach ($datas['temp'] as $temp_all)
+    @foreach ($temp_all as $key => $temp)
+      <td style="padding: 0px">{{ $temp }}</td>
+    @endforeach
+  @endforeach
+    </tr>
 @endforeach
 </table>
 </div>
