5e88b370dbad378fb4b265d1672186596224248f
TakuyaNagai <nagataku021@outlook.jp>
2020-08-23 21:57:13 +0900

    中央に寄せた

--- resources/views/pdf_test.blade.php
+++ resources/views/pdf_test.blade.php
@@ -3,19 +3,16 @@
 <head>
 <meta charset="utf-8">
 <style type="text/css">
-  /* .title25 { width: 25%; }
-  .title50 { width: 50%; }
-  .left {  text-align: left; }
-  .center {  text-align: center; }
-  .right {  text-align: right; }
-  .color_white { background-color: #fff; }
-  .color_blue  { background-color: #99f; }
-  .font_s { font-size: 8; }
-  .font_m { font-size: 12; } */
+table.border td {
+  border: 1px solid #000000;
+}
+td.padding-row {
+  line-height: 50%;
+  text-align: center;
 </style>
 </head>
 <!-- <body class="color_white"><table border="1" width="100%" cellpadding="10" cellspacing="10"> -->
-<div class="wrapper">
+<div class="">
 <table class="tbl" border="1">
 <tr><th class="num">日付\機器名称</th>
   @foreach ($datas['equipments'] as $equipment)
@@ -33,11 +30,11 @@
 
 @foreach ($datas['date'] as$date)
     <tr>
-      <td>{{ $date }}</td>
+      <td class="padding-row">{{ $date }}</td>
   @foreach ($datas['temp'] as $equipment_id => $temp_all)
     @foreach ($temp_all as $temp_type => $temp)
       @foreach ($temp as $temp_data)
-        <td>{{ $temp_data }}</td>
+        <td class="padding-row"">{{ $temp_data }}</td>
       @endforeach
     @endforeach
   @endforeach
