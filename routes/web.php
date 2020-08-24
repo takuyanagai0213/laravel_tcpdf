7ff1d278f2baa8b3a87655935f16cde4a72e4bf5
TakuyaNagai <nagataku021@outlook.jp>
2020-08-24 22:38:12 +0900

    最初に戻した

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/pdf', 'DocumentController@index');
-Route::post('/pdf', 'DocumentController@postHoge');
